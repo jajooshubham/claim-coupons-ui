9dede73cc7c38e0540353cfab15724c32bdb7bd5
shubhamjajoo <jajooraghu@gmail.com>
2025-05-02 01:37:16 +0530

    agent name

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -232,7 +232,7 @@ export default function Register() {
 												name="agentName"
 												id="agentName"
 												className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-												placeholder="Agent Name"
+												placeholder="Agent Name:"
 												required={true}
 												defaultValue={actionData?.fields?.agentName}
 												aria-invalid={Boolean(actionData?.fieldErrors?.agentName)}
